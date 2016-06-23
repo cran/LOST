153a9f31f5456f5c159d8f1bf998bfee4310fb12
J. Arbour <jarbour@uw.edu>
2016-06-23 00:57:51 +0000

    version 1.2

--- R/complete.specimens.R
+++ R/complete.specimens.R
@@ -1,21 +1,25 @@
 complete.specimens <-
-function(dataset,nlandmarks){
-  base<-c(1,1)
-  included<-base
-  excluded<-base
-  nspecimen<-nrow(dataset)/nlandmarks
-  start<-seq(from=1,to=nrow(dataset),by=nlandmarks)
-  nsp<-1:nspecimen
-  for (k in 1:nspecimen){
-    x<-start[k]
-    y<-x+nlandmarks-1
-    single<-dataset[x:y,]
-    reduced<-na.omit(single)
-    rows<-nrow(reduced)
-    if (rows==nlandmarks){included<-rbind(included,single)}
-    else {excluded<-rbind(excluded,single)}
+function(dataset, nlandmarks) {
+    base <- c(1, 1)
+    included <- base
+    excluded <- base
+    nspecimen <- nrow(dataset)/nlandmarks
+    start <- seq(from = 1, to = nrow(dataset), by = nlandmarks)
+    nsp <- 1:nspecimen
+    for (k in 1:nspecimen) {
+      x <- start[k]
+      y <- x + nlandmarks - 1
+      single <- dataset[x:y, ]
+      reduced <- na.omit(single)
+      rows <- nrow(reduced)
+      if (rows == nlandmarks) {
+        included <- rbind(included, single)
+      }
+      else {
+        excluded <- rbind(excluded, single)
+      }
+    }
+    end <- nrow(included)
+    included <- included[2:end, ]
+    return(included)
   }
-  end<-nrow(included)
-  included<-included[2:end,]
-  return (included)
-}
--- R/how.many.missing.R
+++ R/how.many.missing.R
@@ -1,8 +1,9 @@
 how.many.missing <-
-function(x){
-    totaldata<-nrow(x)*ncol(x)
-    excluded<-ifelse(is.na(x),1,0)
-    sums<-sum(excluded)
-    percent<-sums/totaldata
+function (x) 
+{
+    totaldata <- nrow(x) * ncol(x)
+    excluded <- ifelse(is.na(x), 1, 0)
+    sums <- sum(excluded)
+    percent <- sums/totaldata
     return(percent)
-  }
+}
